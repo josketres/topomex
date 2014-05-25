df8426c316ee0ae3ea8141c1801e8516d4681394
josketres <josketres@gmail.com>
2014-05-25 14:20:30 +0200

    Add example of using mx_states.topo.json with datamaps

--- README.md
+++ README.md
@@ -1,30 +1,26 @@
-topomex
-=======
+# topomex
+
 
 Popular topology encoding formats for maps of Mexico.
 
 * topojson [(https://github.com/mbostock/topojson)]
 
-Maps
---------
-
-Examples
---------
+## Maps
 
 
-Build dependencies
---------
+## Examples
+
+
+## Build (generate the maps data yourself)
+
+### Dependencies
 * Vagrant
 * Virtualbox
 
-Build (generate the maps data yourself)
---------
+### Build
+
     git clone 
     cd topomex/vagrant
     vagrant up
     vagrant ssh
-
-
-
-  
-
+    /topomex/scripts/create_mx_states_json.sh
--- scripts/create_mx_states_json.sh
+++ scripts/create_mx_states_json.sh
@@ -4,9 +4,11 @@ then
     curl -o estados.zip http://mapserver.inegi.org.mx/MGN/mge2010v5_0.zip
 fi
 
-unzip estados.zip
+unzip -u estados.zip
 
 ## shapefile --> topojson (http://datamaps.markmarkoh.com/using-custom-map-data-w-datamaps)
 ogr2ogr states.shp Entidades_2010_5.shp -t_srs "+proj=longlat +ellps=WGS84 +no_defs +towgs84=0,0,0"
+
 ## id-property needed so that DataMaps knows how to color the map (entity name is the id)
 topojson -o /topomex/dist/mx_states.topo.json -s 1e-7 -q 1e5 states.shp -p state_code=+CVE_ENT,name=NOM_ENT --id-property NOM_ENT
+echo "Map data '/topomex/dist/mx_states.topo.json' successfully generated"
